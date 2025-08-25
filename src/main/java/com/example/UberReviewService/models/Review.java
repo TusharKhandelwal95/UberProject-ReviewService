457a28a1c2393ed4c9e07d6db6171a0d43643eae
TusharKhandelwal95 <khandelwaltushar95@gmail.com>
2025-08-25 18:55:56 +0530

    Added passengerReview model as joined table

--- src/main/java/com/example/UberReviewService/models/Review.java
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -9,7 +9,10 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="bookingreview")
+@Table(name="booking_review")
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // this will tell spring to use single table for all child classes of Review class
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // this will create separate table for each child class long with table for Review class as well
+@Inheritance(strategy = InheritanceType.JOINED) // this will create separate table for each child class containing only unique fields of child class and common fields will be stored in parent class table
 public class Review extends BaseModel {
 
     @Column(nullable=false)
