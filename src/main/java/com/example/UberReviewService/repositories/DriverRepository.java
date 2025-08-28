179acf1b718ff6e9ba54e6bb8cb24147b5f9ad8d
TusharKhandelwal95 <khandelwaltushar95@gmail.com>
2025-08-28 20:35:40 +0530

    Added controllers

--- src/main/java/com/example/UberReviewService/repositories/DriverRepository.java
+++ src/main/java/com/example/UberReviewService/repositories/DriverRepository.java
@@ -2,23 +2,10 @@ package com.example.UberReviewService.repositories;
 
 import com.example.UberReviewService.models.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface DriverRepository extends JpaRepository<Driver, Long> {
 
-    Optional<Driver> findByIdAndLicenseNumber(Long id, String licenseNumber);
-
-//    @Query(nativeQuery = true, value = "SELECT * FROM Driver WHERE id = :id AND license_number = :license") // raw mysql query, error is thrown at runtime
-//    Optional<Driver> rawFindByIdAndLicenseNumber(Long id, String license);
-//
-//    @Query("SELECT d FROM Driver d WHERE d.id = :id AND d.licenseNumber = :license") // hibernate query, error is thrown at compile time
-//    Optional<Driver> hqlFindByIdAndLicenseNumber(Long id, String license);
-
-//    @Query(nativeQuery = true, value = "SELECT * FROM Driver d WHERE d.id IN :ids")
-//    List<Driver> rawFindDriversById(List<Long> ids);
-
-    List<Driver> findByIdIn(List<Long> ids);
 }
--- src/main/java/com/example/UberReviewService/services/ReviewService.java
+++ src/main/java/com/example/UberReviewService/services/ReviewService.java
@@ -1,89 +1,22 @@
 package com.example.UberReviewService.services;
 
-import com.example.UberReviewService.models.Booking;
-import com.example.UberReviewService.models.Driver;
 import com.example.UberReviewService.models.Review;
-import com.example.UberReviewService.repositories.BookingRepository;
-import com.example.UberReviewService.repositories.DriverRepository;
-import com.example.UberReviewService.repositories.ReviewRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-
+import java.util.List;
+import java.util.Optional;
 
 @Service
-public class ReviewService implements CommandLineRunner {
+public interface ReviewService {
 
-    private ReviewRepository reviewRepository;
-    private BookingRepository bookingRepository;
-    private DriverRepository driverRepository;
+    public Optional<Review> findReviewById(Long id);
 
-    public ReviewService(ReviewRepository reviewRepository, BookingRepository bookingRepository, DriverRepository driverRepository) {
-        this.reviewRepository = reviewRepository;
-        this.bookingRepository = bookingRepository;
-        this.driverRepository = driverRepository;
-    }
+    public List<Review> findAllReviews();
 
-    @Override
-    @Transactional
-    public void run(String... args) throws Exception {
-        System.out.print("********************************");
+    public boolean deleteReviewById(Long id);
 
-//        Review r = Review
-//                .builder()
-//                .content("Great ride!")
-//                .rating(4.5)
-//                .build();
-//
-//
-////        reviewRepository.save(r); // this code execute sql queries
-////
-////        System.out.println(r.getId());
-//
-//        Booking b = Booking
-//                .builder()
-//                .review(r) // setting the review object to the booking object
-//                .build();
-//
-//        bookingRepository.save(b); // this code execute sql queries
+    public Review publishReview(Review review);
 
-//        Optional<Booking> fetchedBooking = bookingRepository.findById(2L);
-//        if(fetchedBooking.isPresent()){
-//            bookingRepository.delete(fetchedBooking.get());
-//        }
+    public Review updateReview(Long id, Review review);
 
-//        Optional<Driver> fetchedDriver = driverRepository.findByIdAndLicenseNumber(1L, "DL123456");
-//        if(fetchedDriver.isPresent()){
-//            Driver driver = fetchedDriver.get();
-//            System.out.println("Driver Name: " + driver.getName());
-//            List<Booking> bookings = driver.getBookings(); // LAZY loading happens here
-//            for(Booking booking: bookings){
-//                System.out.println(booking.getId());
-//            }
-//        }
-
-        /*
-        * Handling N+1 problem
-        * Suppose for a list of driver id we have to find bookings for all of them
-        * So we would make 1 call to get all driver data then we will make N calls for each driver's booking
-        *
-        * To prevent it we can
-        * 1..  Get all driver(1 call) --> get booking of all drivers using join(2nd call)
-        * 2.. Get all driver(1 call) --> then use @fetch in driver class then use loop.
-        * also make function @Transactional so that session is open till the end of function
-        * */
-        List<Long> driverIds = new ArrayList<>(Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L));
-        List<Driver> drivers = driverRepository.findByIdIn(driverIds);
-
-//        1..
-//        List<Booking> bookings = bookingRepository.findAllByDriverIn(drivers);
-//        2..
-        for(Driver driver : drivers){
-            List<Booking> bookings = driver.getBookings(); // LAZY loading happens here but no n+1 problem because of @Fetch
-            System.out.println(driver.getName() + " : " + bookings.size() + " bookings.");
-            bookings.forEach(booking -> System.out.println(booking.getId()));
-        }
-    }
 }
